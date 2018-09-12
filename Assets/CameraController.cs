f2f8ab24ddb625ab3b1d38538ac215f5608c2e50
Viraj Kaushalye <virajkaush@gmail.com>
2018-09-12 09:05:14 +0530

    Simulator Start, test done

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,20 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
-	private CharacterController cc;
-	public float x = 0.0f;
-	public float y = 0.0f;
-	public float z = 0.0f;
-	public float speed = 10.0f;
+	public float x = 0.0f, y = 1.0f, z = 0.0f;
+	public float speed = 2.0f;
 	private Vector3 target;
 
 	// Use this for initialization
 	void Start ()
 	{
+		Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+			var dependencyStatus = task.Result;
+			if (dependencyStatus == Firebase.DependencyStatus.Available)
+			{
+				// Create and hold a reference to your FirebaseApp, i.e.
+				//   app = Firebase.FirebaseApp.DefaultInstance;
+				// where app is a Firebase.FirebaseApp property of your application class.
+
+				// Set a flag here indicating that Firebase is ready to use by your
+				// application.
+			}
+			else
+			{
+				UnityEngine.Debug.LogError(System.String.Format(
+					"Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+				// Firebase Unity SDK is not safe to use here.
+			}
+		});
+
 		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://estimoteindoor.firebaseio.com/");
 		DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
 
@@ -28,49 +47,62 @@ public class CameraController : MonoBehaviour
 				else if (task.IsCompleted)
 				{
 					DataSnapshot snapshot = task.Result;
-					Debug.Log(snapshot);
-				    target = new Vector3(snapshot.x, 0, snapshot.y);
+					Debug.Log("Setup: " + snapshot.Value);
+					// target = new Vector3(snapshot.x, 0, snapshot.y);
 				}
 			});
-
-        cc = GetComponent<CharacterController>();
+		
 		target = new Vector3(x, y, z);
 	}
 
-    // Update is called once per frame
-    void Update ()
-    {
-        _HandleSimulatorLifecycle();
+	// Update is called once per frame
+	void Update ()
+	{
+		_HandleSimulatorLifecycle();
+		FirebaseDatabase.DefaultInstance
+			.GetReference("locationPosition")
+			.ValueChanged += _HandleValueChanged;
 
-		target = new Vector3(x, y, z);
-        FirebaseDatabase.DefaultInstance
-            .GetReference("locationPosition")
-            .ValueChanged += _HandleValueChanged;
-
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+		// transform.position = target;
+		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
 	}
 
-    
+	
 
-    private void _HandleValueChanged(object sender, ValueChangedEventArgs args)
-    {
-        if (args.DatabaseError != null)
-        {
-            return;
-        }
+	private void _HandleValueChanged(object sender, ValueChangedEventArgs args)
+	{
+		double x = 0.0, y = 0.0;
+		Double xx = 0, yy = 0;
+		if (args.DatabaseError != null)
+		{
+			return;
+		}
 
-        // Do something with the data in args.Snapshot
-        target = new Vector3(args.Snapshot.x, 0, args.Snapshot.y);
-    }
+		foreach (KeyValuePair<string, object> pair in (IEnumerable<KeyValuePair<string, object>>) args.Snapshot.Value)
+		{
+			if (pair.Key == "x")
+			{
+				// x = (double) pair.Value;
+				xx = (Double) pair.Value;
+				Debug.Log(pair.Value.GetType());
+			} else if (pair.Key == "y")
+			{
+				yy = (Double) pair.Value;
+				// y = (Double) pair.Value;
+			}
+		}
 
-    private void _HandleSimulatorLifecycle()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            FirebaseDatabase.DefaultInstance
-                .GetReference("locationPosition")
-                .ValueChanged -= _HandleValueChanged; // unsubscribe from ValueChanged.
-            Application.Quit();
-        }
-    }
+		target = new Vector3((float)xx , 1, (float)yy);
+	}
+
+	private void _HandleSimulatorLifecycle()
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			FirebaseDatabase.DefaultInstance
+				.GetReference("locationPosition")
+				.ValueChanged -= _HandleValueChanged; // unsubscribe from ValueChanged.
+			Application.Quit();
+		}
+	}
 }
